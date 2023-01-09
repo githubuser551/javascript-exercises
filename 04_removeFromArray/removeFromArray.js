980783c8454de968bb895a0ee3242caceb0e92a8
githubuser551 <gamplo101@gmail.com>
2023-01-09 17:49:04 -0500

    Solve 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,12 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array) {
+    for (i = 1; i < arguments.length; i++){
+        for (j = 0; j <= array.length; j++){
+            if(array[j] === arguments[i]){
+                array.splice(j, 1)
+            }
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
