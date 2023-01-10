dcfcde03fb0af2b3c9da3eeba7f160918dadbf2a
githubuser551 <gamplo101@gmail.com>
2023-01-09 19:00:52 -0500

    Solve 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    
+    let sum = 0
 
+    // returns an 'ERROR' if the parameters are non-integers
+    if (Number.isInteger(a) == false || Number.isInteger(b) == false){
+        return "ERROR"
+    }
+
+     // returns 'ERROR' if the parameters are negative
+    if (a < 0 || b < 0){
+        return "ERROR"
+    }
+   
+    // assigns the higher/lower parameter to the appropriate variable
+    var i = a < b ? a: b;
+    var j = a > b ? a: b;
+
+    // increments through adding the totals
+    for (; i <= j; i++) {
+        sum += i
+    }
+    return sum
 };
 
 // Do not edit below this line
