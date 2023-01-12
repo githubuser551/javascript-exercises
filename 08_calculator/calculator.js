199c48249692007ec42107768e688edf8337c0aa
githubuser551 <gamplo101@gmail.com>
2023-01-12 15:38:56 -0500

    Solve 08_calulator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,49 @@
 const add = function() {
-	
+  let sum = 0
+  for (i = 0; i < arguments.length; i++){
+    sum += arguments[i];
+  }
+  return sum
 };
 
 const subtract = function() {
-	
+  let diff = arguments[0] - arguments[1]
+  return diff;
 };
 
-const sum = function() {
-	
+const sum = function(a) {
+	let total = 0
+  for (i = 0; i < a.length; i++){
+    total += a[i];
+  }
+  return total
 };
 
-const multiply = function() {
-
+const multiply = function(a) {
+var prod = 1;
+for (var i = 0; i < a.length; i++) {
+  prod *= a[i];
+}
+return prod;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+  total = a;
+	for (i = 1; i < b; i++){
+    total *= a
+  }
+  return total
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+  fac = n
+  if(n === 0){
+    return 1;
+  }
+  for(i = 1; i < n; i++){
+    fac *= i;
+  }
+  return fac
 };
 
 // Do not edit below this line
