bffb7e9a0a4fb9979a3cebecd011fca8feeeb955
githubuser551 <gamplo101@gmail.com>
2023-01-12 20:28:09 -0500

    Solve 09_palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let final = ""
+    
+    // converts all to lowercase
+    let a = string.toLowerCase();
 
+    // iterates through the string to concat only the letters
+    for(i = 0; i < a.length; i++){
+        if((/[a-zA-Z]/).test(a[i])){
+            final += a[i]
+        }
+    }
+    
+    // cuts in from both ends comparing chars returning false if a mismatch is found
+    for(i = 0; i < final.length; i++){
+        if(final[i] !== final[final.length - i - 1]){
+            return false
+        }
+    }
+
+    // returns true if there are no mismatches
+    return true
 };
 
 // Do not edit below this line
 module.exports = palindromes;
+
+
+// 
