523c88e3507aa7542a9c1d888fed40fa539cc160
githubuser551 <gamplo101@gmail.com>
2023-01-12 21:27:32 -0500

    Solve 10_fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(n) {
+    fibb = [1, 1];
+    // checks for negative argument
+    if(n < 0){
+        return "OOPS"
+    }
 
+    for(i = 1; i < n; i++){
+        fibb.push(fibb[i] + fibb[(i - 1)])
+    }
+    return fibb[n-1]
 };
 
 // Do not edit below this line
