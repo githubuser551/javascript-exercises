4c287d488574f8fe9ec6f2232ba025c73d6e5dba
githubuser551 <gamplo101@gmail.com>
2023-01-13 13:01:07 -0500

    Solve 12_findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,22 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
 
+    let oldest = -1;
+    let oldestAge = 0;
+    
+    for(i = 0; i < arr.length; i++){
+        // sets a temp variable to current year if there is no year of death
+        let tempYod = (arr[i]["yearOfDeath"]) ? arr[i]["yearOfDeath"] : new Date().getFullYear();
+
+        // sets a temp age variable
+        let tempAge = tempYod - (arr[i]["yearOfBirth"]);
+
+        // compares current to oldest person and updates variables if so
+        if (tempAge > oldestAge){
+            oldest = i;
+            oldestAge = tempAge;
+    }
+    }
+    return arr[oldest]
 };
 
 // Do not edit below this line
